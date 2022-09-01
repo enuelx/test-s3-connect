abf3bfe0d4a156c9e462f48c10e9958b4f7e70f0
Santiago Grangetto <santiagograngetto@gmail.com>
2022-09-01 16:08:54 -0300

    fix syn page margins

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,37 +1,36 @@
-import React, { useContext } from "react";
-import { Button, Container, Grid } from "@mui/material";
+import React, { useContext } from 'react';
+import { Container, Grid } from '@mui/material';
 
-import WalletData from "./WalletsInfo";
-import { UserContext, ToastContext } from "@context";
-import { Loader } from "@components";
-import DiscordCard from "./DiscordCard";
+import WalletData from './WalletsInfo';
+import { UserContext, ToastContext } from '@context';
+import { Loader } from '@components';
+import DiscordCard from './DiscordCard';
 
 const Welcome = () => {
   const userContext = useContext(UserContext);
   const toastContext = useContext(ToastContext);
 
   return userContext.user === null ? (
-    "Error loading user"
+    'Error loading user'
   ) : !userContext.user ? (
     <Loader />
   ) : (
     <Container
       style={{
-        backgroundColor: "#252525",
-        width: "100%",
-        height: "100%",
-        minHeight: "100vh",
-        minWidth: "100vw",
-        marginLeft: "2vw",
+        backgroundColor: '#252525',
+        width: '100vw',
+        height: '100vh',
+        minHeight: '100vh',
+        minWidth: '100vw'
       }}
     >
       <Grid
         style={{
-          display: "flex",
-          justifyContent: "center",
-          flexWrap: "wrap",
-          paddingTop: "20vh",
-          marginLeft: "18vw",
+          display: 'flex',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+          paddingTop: '20vh',
+          marginLeft: '18vw'
         }}
       >
         <WalletData userContext={userContext} toastContext={toastContext} />

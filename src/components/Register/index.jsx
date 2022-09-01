ca7660d39ff4827804ea9303a0f5899cea67cff1
Santiago Grangetto <santiagograngetto@gmail.com>
2022-09-01 16:08:46 -0300

    fix register page margins

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -3,9 +3,11 @@ import { Box } from '@mui/material';
 import { accountApi } from '@services';
 import { AccountForm } from '@components';
 import { Container } from '@mui/system';
-import imgChartRegister from '../../style/img/imgChartRegister.png';
+import {
+  registerBackground as background,
+  createAccountText
+} from './style/img';
 import textLogin from '../../style/img/textLogin.png';
-import tituloCreate from '../../style/img/createAccount.png';
 
 import { Link } from 'react-router-dom';
 export default () => {
@@ -13,24 +15,15 @@ export default () => {
     <Container
       sx={{
         marginRight: '0px',
-        height: '980px',
+        height: '100vh',
 
         minWidth: '-webkit-fill-available',
         position: 'relative',
         left: 0,
         top: 0,
-        background: 'linear-gradient(90deg, #2a2d23, #6d705e)'
+        background: `url(${background}), linear-gradient(90deg, #2a2d23, #6d705e)`
       }}
     >
-      <img
-        style={{
-          width: '1720px',
-          position: 'absolute',
-          left: '250px'
-        }}
-        src={imgChartRegister}
-      ></img>
-
       <img
         style={{
           width: '500px',
@@ -50,7 +43,7 @@ export default () => {
           left: '59px',
           marginLeft: '150px'
         }}
-        src={tituloCreate}
+        src={createAccountText}
         alt=""
       />
 

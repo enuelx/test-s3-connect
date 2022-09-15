aaa79b26a57b5e9ff804d129dbf2c9da1dfae2f8
Santiago Grangetto <santiagograngetto@gmail.com>
2022-09-15 10:52:08 -0300

    add special characters

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 export default {
   apiUrl: import.meta.env.VITE_API_URL,
-  passwordRegex:
-    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_=.]).{8,}$/,
+  passwordRegex: new RegExp(
+    '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]).{8,}$'
+  ),
   captchaKey: import.meta.env.VITE_CAPTCHA_SITE_KEY
 };

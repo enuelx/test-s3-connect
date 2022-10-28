1b98188f46bd41f60ab7fedd0b8dcbc0ba97cf97
Santiago Grangetto <sgrangetto@nxastudios.com.ar>
2022-10-28 15:56:20 +0000

    Add connect twitter

--- src/services/index.js
+++ src/services/index.js
@@ -3,3 +3,4 @@ export { default as walletApi } from './walletApi';
 export { default as discordUserApi } from './discordUserApi';
 export { default as shippingAddressApi } from './shippingAddressApi';
 export { default as emailApi } from './emailApi';
+export { default as twitterUserApi } from './twitterUserApi';

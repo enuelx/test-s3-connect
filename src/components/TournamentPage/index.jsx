cf17c7449901f8fd96f58f0257db6b291234f94f
Santiago Grangetto <santiagograngetto@gmail.com>
2022-11-03 14:48:13 -0300

    Update favicon and tournament page title

--- src/components/TournamentPage/index.jsx
+++ src/components/TournamentPage/index.jsx
@@ -56,7 +56,7 @@ export const TournamentPage = () => {
     <Container className="containerTournament">
       <Grid className="gridTournament">
         <div>
-          <Typography variant="h4">Tournament Challengers</Typography>
+          <Typography variant="h4">The Moshpit Tournament Challengers</Typography>
 
           <Typography sx={{ marginTop: '3vh' }}>
             Each Cypher you hodl gives you a tournament entry, meaning you can
